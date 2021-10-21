0c0322941db000fe1c39a40aeccc1ed30f0c791b
HeavenM1 <heaven.mccoy.student@madisonpsb.org>
2021-10-21 15:41:03 +0000

    platformer

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -39,7 +39,7 @@
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
-        createPlatform(760,510);
+        createPlatform(760,100);
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
